038e658e4a46dbf847d2b8e260c9bac73b9b331d
Sudharshika <sudharshika10@gmail.com>
2026-02-05 05:32:27 +0530

    Day 01: Hello World, variables, and addition program

---
+++ Day01/main.py
@@ -0,0 +1,14 @@
+# Problem 1: Print "Hello, World!"
+print("Hello, World!")
+
+# Problem 2: Create variables for name, age, and favorite language, then print them in a sentence.
+name = "John"
+age = 30
+favorite_language = "English"
+print(name, "is ", age,"years old. His favorite language is",favorite_language)
+
+# Problem 3: Write a program to add two numbers entered by the user.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+sum_of_numbers = num1+num2
+print("sum of number a and b is ",sum_of_numbers)
