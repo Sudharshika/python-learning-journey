4318489b25e1e5def68142173a5f6d289fbac564
Sudharshika <sudharshika10@gmail.com>
2026-02-06 18:05:15 +0530

    Day 02: Practiced line breaks, rectangle area calculation, and explored Python data types

---
+++ Day02/main.py
@@ -0,0 +1,22 @@
+# Problem 1: Write a program that prints:
+# Python is fun!
+# Let's code every day.
+print("Python is fun!\nLet's code every day.")
+
+# Problem 2: Write a program to calculate the area of a rectangle 
+rectangle_width = 5
+rectangle_length = 10
+area = rectangle_width * rectangle_length
+print("Area of a rectangle: ",area)
+
+# Problem 3: Create variables of int, float, string, boolean, None Print their values along with their data types
+name = "Joe"
+age = 25
+weight = 47.92
+is_student = False
+has_cars = None
+print("Name: ",name, "| Type: ",type(name))
+print("Age: ",age, "| Type: ",type(age))
+print("Weight: ",weight, "| Type: ",type(weight))
+print("Is student: ",is_student, "| Type: ",type(is_student))
+print("Has cars: ",has_cars, "| Type: ",type(has_cars))
